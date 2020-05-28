f1217586450c406244017fa855da513cf058c2a4
Federico Zacayan <garron.garron@gmail.com>
2020-05-28 19:10:44 -0300

    React Version look and feel

---
+++ build/precache-manifest.96dca3e537d4e1170219840654762173.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "11b38f7eabfb24b8fc14efa67d9a98cf",
+    "url": "./index.html"
+  },
+  {
+    "revision": "fd51a582df660401a368",
+    "url": "./static/css/main.7305623f.chunk.css"
+  },
+  {
+    "revision": "45682774503a88a6e23d",
+    "url": "./static/js/2.bf8c3e12.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "./static/js/2.bf8c3e12.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "fd51a582df660401a368",
+    "url": "./static/js/main.1665b0fe.chunk.js"
+  },
+  {
+    "revision": "9b97190bb6c38a201f41",
+    "url": "./static/js/runtime-main.edbc65b8.js"
+  }
+]);
