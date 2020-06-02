a3bf5f0552cec2ae08b353c8be86bda3f9d19b5e
Federico Zacayan <garron.garron@gmail.com>
2020-06-02 19:10:37 -0300

    PenTool

---
+++ pen/precache-manifest.0cf74b47a0ed29c85067ee9d712e22c8.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "92fbd64bd213063b9fa26f559336bd7f",
+    "url": "./index.html"
+  },
+  {
+    "revision": "b47528e8efe2aa72b720",
+    "url": "./static/css/main.6471f835.chunk.css"
+  },
+  {
+    "revision": "a49807889d0f63b6b114",
+    "url": "./static/js/2.2a2c0d7f.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "./static/js/2.2a2c0d7f.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "b47528e8efe2aa72b720",
+    "url": "./static/js/main.966ebe76.chunk.js"
+  },
+  {
+    "revision": "9f57e268b0bb37d694b2",
+    "url": "./static/js/runtime-main.79136e0e.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "./static/media/logo.5d5d9eef.svg"
+  }
+]);
