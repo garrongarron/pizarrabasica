0fff0b41d45fa04214af2e49723c1913b56d0287
Federico Zacayan <garron.garron@gmail.com>
2020-06-03 19:07:10 -0300

    Pen and Sings

---
+++ pen2/precache-manifest.a4abed56cbad9453c0782a663868a403.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "bd21fca09c253a4e3dacf1ed70a45b0c",
+    "url": "./index.html"
+  },
+  {
+    "revision": "3d4ff9e0140e6ea0d662",
+    "url": "./static/css/main.e3eb8955.chunk.css"
+  },
+  {
+    "revision": "17e870fa193920205b0c",
+    "url": "./static/js/2.a1290a2e.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "./static/js/2.a1290a2e.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "3d4ff9e0140e6ea0d662",
+    "url": "./static/js/main.6cbd299e.chunk.js"
+  },
+  {
+    "revision": "9f57e268b0bb37d694b2",
+    "url": "./static/js/runtime-main.79136e0e.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "./static/media/logo.5d5d9eef.svg"
+  }
+]);
